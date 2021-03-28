7a3d203aa198ae7c5af0cd37f220e03004cc6694
MaksymPashkovskyi <pashkovskymax.mp@gmail.com>
2021-03-28 21:35:02 +0300

    fixed empty lines and ';'

--- test/extended/age.js
+++ test/extended/age.js
@@ -1,15 +1,11 @@
-import exp from '../../data/expected.json'
-import sel from '../../data/selectors'
+import exp from '../../data/expected.json';
+import sel from '../../data/selectors';
 
 describe('Age field test', function () {
 
     before('Open App', function () {
         browser.url('');
     });
-
-
-
-
 
     beforeEach(() => {
         browser.refresh();
